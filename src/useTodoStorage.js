c54d3cf01fddb7a6f9b2ac90ced6b89a42399cda
LAPTOP-E3NOM2LT\monic <monica.winnie3@gmail.com>
2022-06-10 09:42:50 +0700

    refactor: useTodoStorage

--- src/useTodoStorage.js
+++ src/useTodoStorage.js
@@ -1,34 +1,29 @@
-import { useEffect, useRef, useState } from "react"
-
-
+import { useEffect, useRef, useState } from "react";
 
 function useTodoStorage(
     key,
     defaultValue = [],
-    { serialize = JSON.stringify, deserialize = JSON.parse } = {},
+    { serialize = JSON.stringify, deserialize = JSON.parse } = {}
 ) {
     const [state, setState] = useState(() => {
-        const valueInLocalStorage = window.localStorage.getItem(key)
+        const valueInLocalStorage = window.localStorage.getItem(key);
         if (valueInLocalStorage) {
-            return deserialize(valueInLocalStorage)
+            return deserialize(valueInLocalStorage);
         }
-        return defaultValue
-    })
-    console.log('state', state)
+        return defaultValue;
+    });
 
-    const prevKeyRef = useRef(key)
-    console.log(prevKeyRef)
+    const prevKeyRef = useRef(key);
     useEffect(() => {
-        const prevKey = prevKeyRef.current
+        const prevKey = prevKeyRef.current;
         if (prevKey !== key) {
-            window.localStorage.removeItem(prevKey)
+            window.localStorage.removeItem(prevKey);
         }
-        prevKeyRef.current = key
-        console.log("after", prevKeyRef)
-        window.localStorage.setItem(key, serialize(state))
-    }, [key, state, serialize])
+        prevKeyRef.current = key;
+        window.localStorage.setItem(key, serialize(state));
+    }, [key, state, serialize]);
 
-    return [state, setState]
+    return [state, setState];
 }
 
-export { useTodoStorage }
+export { useTodoStorage };
