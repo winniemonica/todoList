df6b4ece7033d9327a6f0de6a05b5c48c2d8ea83
LAPTOP-E3NOM2LT\monic <monica.winnie3@gmail.com>
2022-06-10 09:34:28 +0700

    refactor: delete console log and apply prettier code

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,19 +1,26 @@
 function TodoItem({ id, name, isComplete, onToggle, onDelete }) {
-    console.log(id)
     return (
         <ul className="list-group ">
-            <div className=" d-flex border align-items-center justify-content-between p-2 mx-5 list-box" >
+            <div className=" d-flex border align-items-center justify-content-between p-2 mx-5 list-box">
                 <div>
-                    <li className={`${isComplete ? "text-decoration-line-through" : null}`} onClick={() => onToggle(id)}>
-                        <input type={"checkbox"} className="mx-5" onChange={() => { }} checked={isComplete} />{name}
+                    <li
+                        className={`${isComplete ? "text-decoration-line-through" : null}`}
+                        onClick={() => onToggle(id)}
+                    >
+                        <input
+                            type={"checkbox"}
+                            className="mx-5"
+                            onChange={() => { }}
+                            checked={isComplete}
+                        />
+                        {name}
                     </li>
                 </div>
-                <span className="" onClick={() => onDelete(id)}>X</span>
+                <span className="" onClick={() => onDelete(id)}>
+                    X
+                </span>
             </div>
         </ul>
-
-
-
-    )
+    );
 }
 export default TodoItem;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,46 +1,46 @@
-import { useTodoStorage } from "../useTodoStorage"
-import TodoForm from "./TodoForm"
-import TodoItem from "./TodoItem"
+import { useTodoStorage } from "../useTodoStorage";
+import TodoForm from "./TodoForm";
+import TodoItem from "./TodoItem";
 
 function TodoList() {
-    const [todos, setTodos] = useTodoStorage("todos")
+    const [todos, setTodos] = useTodoStorage("todos");
 
     function onSubmit(todo) {
         if (todo === "") {
-            return
+            return;
         }
-        setTodos([...todos, { id: new Date().valueOf(), name: todo, isComplete: false }])
+        setTodos([
+            ...todos,
+            { id: new Date().valueOf(), name: todo, isComplete: false },
+        ]);
     }
 
     function toggleTodo(id) {
-        const updateTodos = todos.map(todo => (todo.id === id) ? { ...todo, isComplete: !todo.isComplete } : todo)
-        setTodos([...updateTodos])
+        const updateTodos = todos.map((todo) =>
+            todo.id === id ? { ...todo, isComplete: !todo.isComplete } : todo
+        );
+        setTodos([...updateTodos]);
     }
     function deleteTodo(id) {
-        const updateTodos = todos.filter(todo => todo.id !== id)
-        setTodos([...updateTodos])
+        const updateTodos = todos.filter((todo) => todo.id !== id);
+        setTodos([...updateTodos]);
     }
-    const renderTodoItem = (items) => items.map((d) => (
-        <TodoItem
-            key={d.id}
-            {...d}
-            onToggle={toggleTodo}
-            onDelete={deleteTodo}
-        />
-    ))
+    const renderTodoItem = (items) =>
+        items.map((d) => (
+            <TodoItem key={d.id} {...d} onToggle={toggleTodo} onDelete={deleteTodo} />
+        ));
 
-    const complete = renderTodoItem(todos.filter(d => d.isComplete === false))
-    const inComplete = renderTodoItem(todos.filter(d => d.isComplete === true))
+    const complete = renderTodoItem(todos.filter((d) => d.isComplete === false));
+    const inComplete = renderTodoItem(todos.filter((d) => d.isComplete === true));
 
     return (
-        <div >
+        <div>
             <h1 className="my-3">Todo List</h1>
             <TodoForm onSubmit={onSubmit} />
             {complete}
             <div className="py-3"></div>
             {inComplete}
-
         </div>
-    )
+    );
 }
-export default TodoList
+export default TodoList;
